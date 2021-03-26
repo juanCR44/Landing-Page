26315d40986a25e2073698d0bcc30e8a4a2a8605
juanCR44 <juancastror.1235@gmail.com>
2021-03-25 19:21:53 -0500

    Subiendo pruebita

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,132 @@ import {
   Code,
   Grid,
   theme,
+  ColorModeProvider,
+  Flex,
+  Image,
+  Center,
+  Wrap,
+  WrapItem,
+  IconButton,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  GridItem,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button
 } from "@chakra-ui/react"
+
+import IconosCandidatos from "./iconos-candidatos"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
 
+interface user{
+  dni:string
+}
+
+function retrieveDNI(dni:any){
+  console.log(dni);
+}
+function prueba(){
+  console.log("jeje")
+}
+
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+
+  <ChakraProvider theme = {theme}>
+    <Flex  justifyContent = "center" backgroundColor = "gray.100"  height = "100vh">
+      <Box marginTop = "50px"  boxShadow="dark-lg" p="0" rounded="md"  position = "absolute">
+        <Box
+          height = "620px"
+          width = "1000px"
+          backgroundImage = "url(https://i.pinimg.com/originals/06/55/80/065580938b36e4c049ec033745248216.jpg)"
+          padding = "15px"
+          position = "relative"
+        >
+          <Grid 
+            position = "relative"
+            height = "585px"
+            templateRows = "repeat(3, 1fr)"
+            templateColumns = "repeat(2, 1fr)"
+            gap = {4}>
+              <GridItem rowSpan={1}>
+              <Text 
+                fontSize = "38px"
+                fontWeight = "700"
+                fontFamily = "body">
+                Onpe
+              </Text>
+              </GridItem>
+              <GridItem rowSpan={2} colSpan={1}>
+                <Center marginTop="50px">
+                  <Image src = "/images/anfora.png" alt = "" boxSize = "320px">
+                  </Image>
+                </Center> 
+              </GridItem>
+              <GridItem rowSpan={3} colSpan={1}>
+                <Box padding = "10px" color = "white">
+                  <Text fontSize = "28px" fontWeight = "600">Elecciones generales 2021</Text>
+                </Box>
+                <Box padding = "10px" color = "white">
+                  <Text>Ingrese su número de DNI para empezar a votar</Text>
+                </Box>
+                <Box padding = "10px" color = "black">
+                  <FormControl id = "DNI" display = "inline-flex">
+                    <Input type="text" placeholder = "DNI" variant="outline" bg = "white" ref={(c) => retrieveDNI(c)}/>
+                    <Button marginLeft = "10px" bg = "cyan.200" type="submit" onClick = {()=>prueba()}>
+                      Continuar
+                    </Button>
+                  </FormControl>
+                </Box>
+              </GridItem>
+          </Grid>
+        </Box>
+        <Box backgroundColor = "white" height= "250px" padding = "10px" position = "relative">
+          <Text 
+            color = "black" 
+            position = "relative"
+            fontFamily = "serif"
+            fontSize = "26px"
+            fontWeight = "bold">
+              Candidatos
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+          <Box 
+            overflowY="auto" 
+            height = "190px" 
+            width= "980px" 
+            outline = "auto" 
+            outlineColor = "gray.200" 
+            padding = "5px" 
+            bg = "orange.400"
+            css={{
+              '&::-webkit-scrollbar': {
+                width: '12px',
+              },
+              '&::-webkit-scrollbar-track': {
+                width: '6px',
+                background: "#f1f1f1"
+              },
+              '&::-webkit-scrollbar-thumb': {
+                background: "lightblue",
+                borderRadius: '24px',
+              },
+              }
+            }>
+            <IconosCandidatos/>
+          </Box>
+        </Box>
+      </Box>
+    </Flex>
+
   </ChakraProvider>
 )
+
